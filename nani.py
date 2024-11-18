0c8999c5f041e1940cbe41055ae98fb236fe0f41
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-18 18:41:10 -0500

    Second

---
+++ nani.py
@@ -0,0 +1,16 @@
+# fname = input('Enter the file name: ')
+# try:
+#     fhand = open (fname)
+# except:
+#     print('File cannot be opened, please check the file name:', fname)
+#     quit()
+
+# count=0
+# for line in fhand:
+#     if line.startswith('The ') :
+#         count = count + 1
+#     print ('There were', count, 'subject lines in', fname)
+
+s = "Nani"
+print(s[0])
+s[0]="n"
