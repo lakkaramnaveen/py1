fa71349c834a68253780fa6eadeaa0d42bf14a18
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 13:56:11 -0500

    words

--- ch_9/ex_09.py
+++ ch_9/ex_09.py
@@ -6,6 +6,8 @@ for line in hand:
     line = line.rstrip()
     #  strip all spaces intro
     # print(line)
-    words = line.strip()
-    if len(words)>0:
-        print(words[0])
+    words = line.split()
+    # if len(words)>0:
+    #     print(words[0])
+    for word in words:
+        print(word)
