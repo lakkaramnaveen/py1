f38e242d232b4c31dc9de4c313c306650b2890ef
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 12:52:25 -0500

    dict

--- nani.py
+++ nani.py
@@ -1,6 +1,10 @@
-h = dict()
-h[0]= 'nani'
-h[2]= 'asa'
-h[1]= 'nani'
-h[3]= 'nani'
-print(h)
+counts = dict()
+
+print('Enter a line of text: ')
+line = input('')
+names = line.split()
+
+for name in names:
+        counts [name] = counts.get(name,0) + 1
+
+print (counts)
