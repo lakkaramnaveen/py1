120969969c298b39cdc6e08c6815cd1a7f34cf46
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 07:20:28 -0500

    mbox

--- nani.py
+++ nani.py
@@ -2,9 +2,6 @@ han = open('mbox.txt')
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    print(wds)
-    if len(wds)==0:
-        continue
-    if wds[0]!='From':
+    if len(wds)<3 or wds[0]!='From':
         continue
     print(wds[2])
