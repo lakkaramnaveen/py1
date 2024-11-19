f8f1a0eb18abce9cdc875c8d6b0e66feb068f65d
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 13:39:18 -0500

    dict with files

--- nani.py
+++ nani.py
@@ -1,10 +1,18 @@
+name = input('Enter file: ')
+handle = open(name)
+
 counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
 
-print('Enter a line of text: ')
-line = input('')
-names = line.split()
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword  = word
+        bigcount = count
 
-for name in names:
-        counts [name] = counts.get(name,0) + 1
 
-print (counts)
+print(bigword, bigcount)
