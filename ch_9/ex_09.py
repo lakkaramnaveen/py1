bbb9fcbfaa1897e9a49cb1568646798d906f40ce
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 14:39:13 -0500

    counting

--- ch_9/ex_09.py
+++ ch_9/ex_09.py
@@ -2,12 +2,15 @@ fname = input('Enter file: ')
 if len(fname) < 1: fname = 'clown.txt'
 hand = open(fname)
 
+di = dict()
 for line in hand:
     line = line.rstrip()
     #  strip all spaces intro
-    # print(line)
     words = line.split()
-    # if len(words)>0:
-    #     print(words[0])
     for word in words:
-        print(word)
+        # if the key is not there then the count is zero
+        # if the word is not in "di" then assign it zero
+        di[word] = di.get(word, 0) + 1
+
+for w, i in di.items():
+    print(w, i)
