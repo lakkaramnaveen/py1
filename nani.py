f7ca00890742af5e33ab4651aac53f5437598bd0
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 06:29:25 -0500

    split

--- nani.py
+++ nani.py
@@ -1,5 +1,10 @@
-x = "hi how are you"
-stuff = x.split()
+x = "hi;show; are you"
+stuff = x.split(';')
 print(stuff)
 print(len(stuff))
 print(stuff[0])
+
+
+line = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+words = line.split()
+print (words)
