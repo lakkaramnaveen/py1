c5e7b4f266f1d85868d68192358c039f7441428f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-19 16:13:50 -0500

    Tuples

--- nani.py
+++ nani.py
@@ -1,18 +1,7 @@
-name = input('Enter file: ')
-handle = open(name)
-
-counts = dict()
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word,0) + 1
-
-bigcount = None
-bigword = None
-for word, count in counts.items():
-    if bigcount is None or count > bigcount:
-        bigword  = word
-        bigcount = count
-
-
-print(bigword, bigcount)
+x = ('Glenn', 'Sally', 'Joseph')
+print(x[2])
+y = ( 1, 9, 2)
+print(y)
+print(max (y))
+(x,y) = ((1,2,3),('nani','chinni'))
+print(x)
