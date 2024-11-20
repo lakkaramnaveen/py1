2bc6ee843a2ecdeaf42a99f917e65728215edec7
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-20 08:17:20 -0500

    startswith

--- nani.py
+++ nani.py
@@ -7,13 +7,13 @@
 hand = open('mbox.txt')
 for line in hand:
     line = line.rstrip ()
-    if line.startswith('From: ') >= 0:
+    if line.startswith('From: '):
         print (line)
 
-import re
+# import re
 
-hand = open('mbox.txt')
-for line in hand:
-    line = line.rstrip ()
-    if re.search('^From: ', line):
-        print (line)
+# hand = open('mbox.txt')
+# for line in hand:
+#     line = line.rstrip ()
+#     if re.search('^From: ', line):
+#         print (line)
