fd4c1fe25bcffdaae1f1b52aa41eb7c3ef7a6486
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-20 15:08:00 -0500

    double split

--- nani.py
+++ nani.py
@@ -1,8 +1,3 @@
-import re
-hand = open('mbox.txt')
-y = "From: the beginning:, steve@ucmo.edu is it 12 or 19. Where are you From: I amm from india"
-k = re.findall('[0-9]+', y)
-k = re.findall('[ae]+', y)
-k = re.findall('^F+?:', y) # non greedy
-k = re.findall('\S+@\S+', y)
-print(k)
+y = "From: steve@ucmo.edu is it 12 or 19. Where are you From: I amm from india"
+words = y.split()[1].split('@')
+print(words[1])
