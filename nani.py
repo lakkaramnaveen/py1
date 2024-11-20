a09b2143352eda298a76ec4cc111435ee81e0ad9
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-20 11:05:11 -0500

    findall patterns

--- nani.py
+++ nani.py
@@ -4,6 +4,9 @@ hand = open('mbox.txt')
 #     line = line.rstrip ()
 #     if re.search('^X-\S+:', line):
 #         print (line)
-y = "hi how are you doing today what is the time is it 12 or 19"
+y = "From: the beginning:, steve@ucmo.edu is it 12 or 19. Where are you From: I amm from india"
 k = re.findall('[0-9]+', y)
+k = re.findall('[ae]+', y)
+k = re.findall('^F+?:', y) # non greedy
+k = re.findall('\S+@\S+', y) # non greedy
 print(k)
