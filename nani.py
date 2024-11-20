8fa94f1b4daccc49616c858aa43078c0218d4710
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-20 10:35:30 -0500

    findall, search

--- nani.py
+++ nani.py
@@ -1,19 +1,9 @@
-# hand = open('mbox.txt')
-# for line in hand:
-#     line = line.rstrip ()
-#     if line.find('From: ') >= 0:
-#         print (line)
-
+import re
 hand = open('mbox.txt')
-for line in hand:
-    line = line.rstrip ()
-    if line.startswith('From: '):
-        print (line)
-
-# import re
-
-# hand = open('mbox.txt')
 # for line in hand:
 #     line = line.rstrip ()
-#     if re.search('^From: ', line):
+#     if re.search('^X-\S+:', line):
 #         print (line)
+y = "hi how are you doing today what is the time is it 12 or 19"
+k = re.findall('[0-9]+', y)
+print(k)
