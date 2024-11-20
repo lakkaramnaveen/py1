1ff76ba9a4e7a82c592874d5a11e1eb5a11b49ea
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-20 17:47:55 -0500

    tcp/ip

--- nani.py
+++ nani.py
@@ -1,5 +1,12 @@
-import re
+import socket
+import http
 
-x = 'We just received $10.00 for cookies.'
-y = re.findall('\$[0-9.]?+',x)
-print(y)
+# it's establishing a connection between two parties, similar to making a call over the phone
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+# it's like set of guildlines or rules to follow
+
+http.client.ACCEPTED
+
+# protocol, host, document
+# http://www.nani.com/hello.html
