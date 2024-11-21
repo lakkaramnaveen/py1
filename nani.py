a60bd49a0bab69b3b4fb49ef03eab4e131a498cc
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-21 08:52:19 -0500

    s

--- nani.py
+++ nani.py
@@ -1,15 +1,15 @@
 import socket
 # it's establishing a connection between two parties, similar to making a call over the phone
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org',80))
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sock.connect(('data.pr4e.org',80))
 # it's like set of guildlines or rules to follow
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
-mysock.send(cmd)
+my_sock.send(cmd)
 
 while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+    data = my_sock.recv(512)
+    if (len(data) < 1):
         break
     print(data.decode())
 
-mysock.close()
+my_sock.close()
